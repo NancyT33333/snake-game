dbfa420e1c56368a3e0e24250334d1c461f130fe
Anna Tuzova <nancyt33333@gmail.com>
2019-10-30 20:53:51 +0300

    Updates

---
+++ precache-manifest.78f9d49de54e34974f90ebab61b90567.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ca5e0a15285916f22269765ff58114",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "05e25ea434debfb32b3e",
+    "url": "/snake-game/static/css/main.86d981f4.chunk.css"
+  },
+  {
+    "revision": "66306e4f274e6cafac80",
+    "url": "/snake-game/static/js/2.419bf540.chunk.js"
+  },
+  {
+    "revision": "05e25ea434debfb32b3e",
+    "url": "/snake-game/static/js/main.af6522c1.chunk.js"
+  },
+  {
+    "revision": "d67f4a21b5adaddd906a",
+    "url": "/snake-game/static/js/runtime-main.2748ad92.js"
+  }
+]);
